f01528473c1accfaa379d20e2f3b623a4b12f9ef
Jefferson Cargua <jeffersoncargua@gmail.com>
2024-03-26 12:41:10 -0500

    Se resolvio el conflicto con ProductCard.css e index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
-<<<<<<< HEAD
 import { CartProvider } from './context/CartContext';
-=======
->>>>>>> b373ceea60e3e307791f0aa3d0213875bc242d93
 import './index.css';
 import App from './App';
 
@@ -12,16 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-<<<<<<< HEAD
       <CartProvider>
         <App />
       </CartProvider>      
     </Router>
   </React.StrictMode>
 );
-=======
-      <App />
-    </Router>
-  </React.StrictMode>
-);
->>>>>>> b373ceea60e3e307791f0aa3d0213875bc242d93
